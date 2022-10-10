11fd02ae9349f93b4821a394ca7860af5e2d1859
Aman <aman.saxena757@gmail.com>
2022-10-10 08:06:48 +0300

    some regex in files

--- Python-scripts/File_Scripts/Find_emails_from_filep.py
+++ Python-scripts/File_Scripts/Find_emails_from_filep.py
@@ -8,6 +8,7 @@ str = open("D:\\r@at\\test.txt","r").read()
 + as many times as possible
 \. match the symbol, here it's full stop
 {2,4} match atlease 2 times but no more than 4 times
+refer: https://www.enjoyalgorithms.com/blog/regular-expressions-in-ml
 '''
 match = re.findall('[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,4}', str)
 print(match)
